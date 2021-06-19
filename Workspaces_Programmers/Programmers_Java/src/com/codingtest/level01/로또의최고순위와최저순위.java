492b477c0e9a625fbc56ee1d21f4766c28c01d64
KangCine <kwg940930@gmail.com>
2021-06-20 00:09:38 +0900

    로또의 최고 순위와 최저 순위

---
+++ Workspaces_Programmers/Programmers_Java/src/com/codingtest/level01/로또의최고순위와최저순위.java
@@ -0,0 +1,74 @@
+// 로또의 최고 순위와 최저 순위
+package com.codingtest.level01;
+
+import java.util.Arrays;
+
+public class 로또의최고순위와최저순위 {
+	
+	public static void main(String[] args) {
+		
+		int [] lottos = {44, 1, 0, 0, 31, 25};
+		int [] win_nums = {31, 10, 45, 1, 6, 19};
+		
+		Soulution로또의최고순위와최저순위 sol = new Soulution로또의최고순위와최저순위();
+		System.out.println(Arrays.toString(sol.solution(lottos, win_nums)));
+	}
+}
+
+class Soulution로또의최고순위와최저순위{
+    public int[] solution(int[] lottos, int[] win_nums) {
+    	
+    	int count = 0;
+        int min = 0;
+        int max = 0;
+        
+        for(int i = 0; i < lottos.length; i++) {
+        	for(int j = 0; j < win_nums.length; j++) {
+        		if(lottos[i] == win_nums[j]) {
+        			count++;
+        		}
+        	}
+        }
+        
+        for(int i : lottos) {
+        	if(i == 0) {
+        		max++;
+        	}
+        }
+        
+        switch(count) {
+        case 0: min = 6; case 1: min = 6;
+        break;
+        case 2: min = 5;
+        break;
+        case 3: min = 4;
+        break;
+        case 4: min = 3;
+        break;
+        case 5: min = 2;
+        break;
+        case 6: min = 1;
+        break;
+        }
+        
+        switch(count + max) {
+        case 0: max = 6; case 1: max = 6;
+        break;
+        case 2: max = 5;
+        break;
+        case 3: max = 4;
+        break;
+        case 4: max = 3;
+        break;
+        case 5: max = 2;
+        break;
+        case 6: max = 1;
+        break;
+        }
+        
+        
+        int[] answer = {max, min};
+        
+        return answer;
+    }
+}
